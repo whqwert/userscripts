9e191908c8c999252bea8797322d7af0d423d645
whqwert <94757998+whqwert@users.noreply.github.com>
2021-11-26 17:26:15 -0600

    Format scripts

--- Bandcamp-Downloader.user.js
+++ Bandcamp-Downloader.user.js
@@ -14,53 +14,55 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
-    const table = document.querySelectorAll('#track_table > tbody > tr');
-    const adata = unsafeWindow.TralbumData || false;
+	'use strict';
+	const table = document.querySelectorAll('#track_table > tbody > tr');
+	const adata = unsafeWindow.TralbumData || false;
 
-    if (adata) {
-        const albutton = document.querySelector('.share-collect-controls > ul');
-        const downloadButtonHTML =
-            `<li id="download-button">
-                  <span class="bc-ui2" style="height: 12px; width: 15px; display: inline-block; background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0ODcuMTU2IiBoZWlnaHQ9IjQ4Ny4xNTYiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDQ4Ny4xNTYgNDg3LjE1NiIgZmlsbD0iIzY2NiIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHBhdGggZD0iTTIuMTY2IDM3NC41OTZ2MTEyLjU2SDQ4NC45OVYzNzEuNjM1aC00NC40MzF2NzEuMDlINDYuNTk4di02OC4xMjl6Ii8+PHBhdGggZD0ibTI1OS45MjQgMzk3Ljk0MSAxMTQuNjE1LTE3My43OTItMzkuNzg0LjAzQzMzNC42MDQgMjQuNjg0IDE0MC4yODMgMCAxNDAuMjgzIDBzNDQuNDIgNjIuMjA4IDQ0LjU0NCAyMjQuMjkybC0zOS43ODQuMDI5IDExNC44ODEgMTczLjYyeiIvPjwvc3ZnPg==); background-size: contain">
-                  </span>
-                  <span class="share-embed-label">
-                        <button type="button">
-                              Download
-                        </button>
-                  </span>
-            </li>`;
-        function downloadSong(file, title) {
-            GM_download({
-                url: file,
-                name: adata.artist + ' - ' + title + '.mp3',
-                saveAs: true
-            });
-        }
-        if (table.length) { // isAlbum
-            const dlinks = document.getElementsByClassName('dl_link');
-            adata.trackinfo.forEach((track, i) => {
-                const link = dlinks[i];
-                const file = track.file['mp3-128'];
-                const title = track.title;
+	if (adata) {
+		const albutton = document.querySelector('.share-collect-controls > ul');
+		const downloadButtonHTML = `<li id="download-button">
+            <span class="bc-ui2" style="height: 12px; width: 15px; display: inline-block; background-image: url(
+                    data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0ODcuMTU2IiBoZWlnaHQ9IjQ4Ny4xNTYiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDQ4Ny4xNTYgNDg3LjE1NiIgZmlsbD0iIzY2NiIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHBhdGggZD0iTTIuMTY2IDM3NC41OTZ2MTEyLjU2SDQ4NC45OVYzNzEuNjM1aC00NC40MzF2NzEuMDlINDYuNTk4di02OC4xMjl6Ii8+PHBhdGggZD0ibTI1OS45MjQgMzk3Ljk0MSAxMTQuNjE1LTE3My43OTItMzkuNzg0LjAzQzMzNC42MDQgMjQuNjg0IDE0MC4yODMgMCAxNDAuMjgzIDBzNDQuNDIgNjIuMjA4IDQ0LjU0NCAyMjQuMjkybC0zOS43ODQuMDI5IDExNC44ODEgMTczLjYyeiIvPjwvc3ZnPg==
+                ); background-size: contain">
+            </span>
+            <span class="share-embed-label">
+                <button type="button">
+                    Download
+                </button>
+            </span>
+        </li>`;
+		function downloadSong(file, title) {
+			GM_download({
+				url: file,
+				name: adata.artist + ' - ' + title + '.mp3',
+				saveAs: true
+			});
+		}
+		if (table.length) {
+			// isAlbum
+			const dlinks = document.getElementsByClassName('dl_link');
+			adata.trackinfo.forEach((track, i) => {
+				const link = dlinks[i];
+				const file = track.file['mp3-128'];
+				const title = track.title;
 
-                link.innerHTML =
-                    `<a
-                        href="${file}"
-                        title="${adata.artist + ' - ' + title}">
-                        download
-                    </a>`;
-                link.firstChild.onclick = () => {
-                    downloadSong(file, title);
-                    return false;
-                }
-            })
-        } else { // isSong
-            const file = unsafeWindow.TralbumData.trackinfo[0].file['mp3-128'];
-            albutton.insertAdjacentHTML('beforeend', downloadButtonHTML);
-            document.getElementById('download-button').onclick = () => {
-                downloadSong(file, adata.current.title);
-            }
-        }
-    }
+				link.innerHTML = `<a
+                    href="${file}"
+                    title="${adata.artist + ' - ' + title}">
+                    download
+                </a>`;
+				link.firstChild.onclick = () => {
+					downloadSong(file, title);
+					return false;
+				};
+			});
+		} else {
+			// isSong
+			const file = unsafeWindow.TralbumData.trackinfo[0].file['mp3-128'];
+			albutton.insertAdjacentHTML('beforeend', downloadButtonHTML);
+			document.getElementById('download-button').onclick = () => {
+				downloadSong(file, adata.current.title);
+			};
+		}
+	}
 })();
--- Old-Reddit-Link-Fixer.user.js
+++ Old-Reddit-Link-Fixer.user.js
@@ -12,55 +12,61 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
+	'use strict';
 
-    function removeBackslashes(a) {
-        a.href = a.href.replace(/%5C(_|")/g, "$1");
-        a.innerText = a.innerText.replace(/\\(_|")/g, "$1");
-    }
+	function removeBackslashes(a) {
+		a.href = a.href.replace(/%5C(_|")/g, '$1');
+		a.innerText = a.innerText.replace(/\\(_|")/g, '$1');
+	}
 
-    function fixLinks() {
-        // Posts
-        document.querySelectorAll('#siteTable > .thing > .entry > .expando > form > .usertext-body > .md a[href*="%5C"]')
-            .forEach(a => {
-                removeBackslashes(a);
-            })
-        // Comments
-        document.querySelectorAll('.commentarea > .sitetable > .thing a[href*="%5C"]')
-            .forEach(a => {
-                removeBackslashes(a);
-            })
-    }
+	function fixLinks() {
+		// Posts
+		document
+			.querySelectorAll(
+				'#siteTable > .thing > .entry > .expando > form > .usertext-body > .md a[href*="%5C"]'
+			)
+			.forEach(a => {
+				removeBackslashes(a);
+			});
+		// Comments
+		document
+			.querySelectorAll(
+				'.commentarea > .sitetable > .thing a[href*="%5C"]'
+			)
+			.forEach(a => {
+				removeBackslashes(a);
+			});
+	}
 
-    function observeNewComments() {
-        if (document.querySelector('.commentarea > .sitetable')) {
-            new MutationObserver(function () {
-                this.disconnect();
-                fixLinks();
-                addMoreButtonListener();
-            }).observe(document.querySelector('.commentarea > .sitetable'), {
-                childList: true
-            })
-        }
-    }
+	function observeNewComments() {
+		if (document.querySelector('.commentarea > .sitetable')) {
+			new MutationObserver(function () {
+				this.disconnect();
+				fixLinks();
+				addMoreButtonListener();
+			}).observe(document.querySelector('.commentarea > .sitetable'), {
+				childList: true
+			});
+		}
+	}
 
-    function addMoreButtonListener() {
-        let more = document.querySelector('a[id^="more"]');
-        if (more) {
-            more.addEventListener('click', function () {
-                observeNewComments();
-            });
-        }
-    }
+	function addMoreButtonListener() {
+		let more = document.querySelector('a[id^="more"]');
+		if (more) {
+			more.addEventListener('click', function () {
+				observeNewComments();
+			});
+		}
+	}
 
-    new MutationObserver(function () {
-        if (document.getElementsByClassName('commentarea').length) {
-            this.disconnect();
-            fixLinks();
-            addMoreButtonListener();
-        }
-    }).observe(document, {
-        subtree: true,
-        childList: true
-    });
+	new MutationObserver(function () {
+		if (document.getElementsByClassName('commentarea').length) {
+			this.disconnect();
+			fixLinks();
+			addMoreButtonListener();
+		}
+	}).observe(document, {
+		subtree: true,
+		childList: true
+	});
 })();
