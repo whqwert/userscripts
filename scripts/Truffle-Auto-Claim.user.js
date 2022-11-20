2e268a35d8a92fc1ca0a8f126fafca4d8fddcd53
whqwert <94757998+whqwert@users.noreply.github.com>
2022-11-19 22:42:33 -0600

    Add Truffle.TV channel points claimer

---
+++ scripts/Truffle-Auto-Claim.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Truffle.TV Auto Claimer
+// @namespace    https://github.com/whqwert/userscripts
+// @version      1.0.0
+// @description  Auto-claims Truffle channel points
+// @author       whqwert
+// @match        https://new.ludwig.social/channel-points
+// @icon         https://cdn.bio/assets/images/branding/logomark.svg
+// @supportURL   https://github.com/whqwert/userscripts/issues
+// @license      MIT
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  new MutationObserver((_, o) => {
+    if (document.getElementById("root")) {
+      o.disconnect();
+      autoClaim();
+    }
+  }).observe(document.body, { childList: true, subtree: true });
+
+  const autoClaim = () => {
+    const shadow = document.getElementById("root").firstChild.shadowRoot;
+
+    new MutationObserver(() => {
+      const claim = shadow.querySelector(".claim");
+
+      if (claim) claim.click();
+    }).observe(shadow, { childList: true, subtree: true });
+  };
+})();
