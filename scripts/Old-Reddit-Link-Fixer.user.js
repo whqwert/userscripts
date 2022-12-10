56eb5491979cbe2e0e67e277f3bf542c44ecf750
whqwert <94757998+whqwert@users.noreply.github.com>
2022-12-09 21:58:20 -0600

    Rework link fixer

--- scripts/Old-Reddit-Link-Fixer.user.js
+++ scripts/Old-Reddit-Link-Fixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Old Reddit Broken Link Fixer
 // @namespace    https://github.com/whqwert/userscripts
-// @version      1.2.5
+// @version      1.2.6
 // @description  Fixes incorrectly escaped characters in links on Old Reddit
 // @author       whqwert
 // @match        https://*.reddit.com/*/*/comments/*
@@ -15,49 +15,41 @@
 (function () {
 	'use strict';
 
-	/*
-	Function that removes the backslashes from links
-
-	Not sure if quotes or dashes are incorrectly escaped anymore,
-	but I've seen a few old posts/comments where they are.
-	*/
+	// Not sure if quotes or dashes are incorrectly escaped anymore,
+	// but I've seen a few old posts/comments where they are.
 	function removeBackslashes(a) {
-		a.href = a.href.replace(/%5C([\-_"])/g, '$1');
-		a.innerText = a.innerText.replace(/\\([\-_"])/g, '$1');
+		a.href = a.href.replace(/%5C([-_"])/g, '$1');
+		a.innerText = a.innerText.replace(/\\([-_"])/g, '$1');
 	}
 
 	// For link in comments and post, remove backslashes in link
 	function fixAllLinks() {
-		document
-			.querySelectorAll(
-				`#siteTable > .thing > .entry > .expando > form > .usertext-body > .md a[href*="%5C"],
-				.commentarea > .sitetable > .thing a[href*="%5C"]`
-			)
-			.forEach(a => {
-				removeBackslashes(a);
-			});
+		for (const link of document.querySelectorAll(
+			`#siteTable > .thing > .entry > .expando > form > .usertext-body > .md a[href*="%5C"],
+			.commentarea > .sitetable > .thing a[href*="%5C"]`
+		)) {
+			removeBackslashes(link);
+		}
 	}
 
-	// When the 'load more comments' button is pressed, it waits for new comments to load and then runs the link fixer
+	// When the 'load more comments' button is pressed,
+	// it waits for new comments to load and then runs the link fixer
 	function observeNewComments() {
+		// Don't do anything if there are no comments
 		const wait = document.querySelector('.commentarea > .sitetable');
-		if (wait) {
-			new MutationObserver(function () {
-				runLinkFixer(this);
-			}).observe(wait, {
-				childList: true
-			});
-		}
+		if (!wait) return;
+
+		new MutationObserver((_, observer) => {
+			runLinkFixer(observer);
+		}).observe(wait, {
+			childList: true
+		});
 	}
 
 	function addMoreButtonListener() {
-		// 'load more comments' button
-		const more = document.querySelector('a[id^="more_"]');
-		if (more) {
-			more.addEventListener('click', function () {
-				observeNewComments();
-			});
-		}
+		document
+			.querySelector('a[id^="more_"]') // 'load more comments' button
+			?.addEventListener('click', observeNewComments());
 	}
 
 	function runLinkFixer(observer) {
@@ -66,15 +58,10 @@
 		addMoreButtonListener();
 	}
 
-	/*
-	Fix links on load
-
-	Used with '@run-at document-start' to fix links as fast as possible
-	*/
-	new MutationObserver(function () {
-		if (document.getElementsByClassName('commentarea').length) {
-			runLinkFixer(this);
-		}
+	// Fix links on load
+	new MutationObserver((_, observer) => {
+		if (!document.getElementsByClassName('commentarea').length) return;
+		runLinkFixer(observer);
 	}).observe(document, {
 		subtree: true,
 		childList: true
