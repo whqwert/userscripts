8077764d3676effd3d9fd2d433c84f385357ca74
whqwert <94757998+whqwert@users.noreply.github.com>
2024-01-27 16:00:44 -0600

    Fix #6, reorder functions, simplify link search

--- scripts/Old-Reddit-Link-Fixer.user.js
+++ scripts/Old-Reddit-Link-Fixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Old Reddit Broken Link Fixer
 // @namespace    https://github.com/whqwert/userscripts
-// @version      1.2.7
+// @version      1.2.8
 // @description  Fixes incorrectly escaped characters in links on Old Reddit
 // @author       whqwert
 // @match        https://*.reddit.com/*/*/comments/*
@@ -15,52 +15,58 @@
 (function () {
 	'use strict';
 
+	// Fix links on load
+	new MutationObserver((_, observer) => {
+		// Wait for comments to exist
+		if (!document.getElementsByClassName('commentarea').length) return;
+
+		runLinkFixer(observer);
+		addMoreButtonListener();
+	}).observe(document, {
+		childList: true,
+		subtree: true
+	});
+
+	function runLinkFixer(observer) {
+		observer.disconnect();
+		fixAllLinks();
+	}
+
+	// For link with backslash, remove backslashes in link
+	function fixAllLinks() {
+		for (const link of document.querySelectorAll('.thing a[href*="%5C"]')) {
+			removeBackslashes(link);
+		}
+	}
+
 	// Not sure if quotes or dashes are incorrectly escaped anymore,
-	// but I've seen a few old posts/comments where they are.
+	// but I've seen a few old posts/comments where they are
 	function removeBackslashes(a) {
 		a.href = a.href.replace(/%5C([-_"])/g, '$1');
 		a.innerText = a.innerText.replace(/\\([-_"])/g, '$1');
 	}
 
-	// For link in comments and post, remove backslashes in link
-	function fixAllLinks() {
-		for (const link of document.querySelectorAll(
-			`#siteTable > .thing > .entry > .expando > form > .usertext-body > .md a[href*="%5C"],
-			.commentarea > .sitetable > .thing a[href*="%5C"]`
-		)) {
-			removeBackslashes(link);
-		}
+	// Hijack 'load more comments' button onclick to run fixer
+	function addMoreButtonListener() {
+		window.morechildren = (function () {
+			const _morechildren = window.morechildren;
+			return function () {
+				const result = _morechildren.apply(this, arguments);
+				observeNewComments();
+				return result;
+			};
+		})();
 	}
 
 	// When the 'load more comments' button is pressed,
 	// it waits for new comments to load and then runs the link fixer
+	const commentObserver = new MutationObserver((_, observer) => {
+		runLinkFixer(observer);
+	});
 	function observeNewComments() {
-		new MutationObserver((_, observer) => {
-			runLinkFixer(observer);
-		}).observe(document.querySelector('.commentarea > .sitetable'), {
-			childList: true
+		commentObserver.observe(document.querySelector('.commentarea > .sitetable'), {
+			childList: true,
+			subtree: true
 		});
 	}
-
-	function addMoreButtonListener() {
-		document
-			.querySelector('a[id^="more_"]') // 'load more comments' button
-			?.addEventListener('click', observeNewComments());
-	}
-
-	function runLinkFixer(observer) {
-		observer.disconnect();
-		fixAllLinks();
-		addMoreButtonListener();
-	}
-
-	// Fix links on load
-	new MutationObserver((_, observer) => {
-		// Do nothing if there are no comments
-		if (!document.getElementsByClassName('commentarea').length) return;
-		runLinkFixer(observer);
-	}).observe(document, {
-		subtree: true,
-		childList: true
-	});
 })();
